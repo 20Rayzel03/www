defe9e9901cff318b438e7e6b27df00d50f2aac7
Julius <101921036+20Rayzel03@users.noreply.github.com>
2025-10-05 15:42:07 +0200

    änderung

--- main/impressum.php
+++ main/impressum.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
-
-<link rel="stylesheet" href="assets/css/rechtliches.css">
-
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Impressum</title>
+    <link rel="stylesheet" href="assets/css/rechtliches.css">
     <link rel="stylesheet" href="css/rechtliches.css">
 </head>
 <body>
