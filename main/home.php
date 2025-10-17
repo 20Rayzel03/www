a57a2ee661e02386086ac9cd8ee69a896e7d4b8f
Julius <101921036+20Rayzel03@users.noreply.github.com>
2025-10-17 20:18:25 +0200

    test

--- main/home.php
+++ main/home.php
@@ -17,6 +17,7 @@
 
   <!-- Seitenspezifisches CSS -->
   <link rel="stylesheet" href="assets/css/home.css">
+  <link rel="stylesheet" href="assets/css/home2.css">
   <link rel="stylesheet" href="assets/css/sozial.css">
 
   
