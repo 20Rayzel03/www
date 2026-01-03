b0796e6dcfc54a5fc26e6b380b4cf417fd6a71fb
Julius <101921036+20Rayzel03@users.noreply.github.com>
2026-01-03 13:47:10 +0100

    styles

--- main/assets/header.php
+++ main/assets/header.php
@@ -1,17 +1,20 @@
 <?php
+$current = basename($_SERVER['PHP_SELF']);
 
+function navActive($file, $current){
+  return $file === $current ? 'active' : '';
+}
 ?>
+
 <nav class="main-nav" role="navigation" aria-label="Hauptnavigation">
   <div class="nav-left">
     <img src="./assets/img/logo.png" alt="Logo" height="40">
   </div>
 
   <ul class="nav-center">
-    <li><a href="home.php">Startseite</a></li>
-    <li><a href="bio.php">Bio</a></li>
-    <li><a href="blog.php">Blog</a></li>
-    <li><a href="kontakt.php">Kontakt</a></li>
+    <li><a href="home.php" class="<?= navActive('home.php', $current) ?>">Startseite</a></li>
+    <li><a href="bio.php" class="<?= navActive('bio.php', $current) ?>">Bio</a></li>
+    <li><a href="blog.php" class="<?= navActive('blog.php', $current) ?>">Blog</a></li>
+    <li><a href="kontakt.php" class="<?= navActive('kontakt.php', $current) ?>">Kontakt</a></li>
   </ul>
-
-  <!-- <a href="login.php" class="login-btn">Login</a> -->
 </nav>
