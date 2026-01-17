67ab9add6acb4c467bc8884aaae2ae979649cdc5
Julius <101921036+20Rayzel03@users.noreply.github.com>
2026-01-17 11:44:09 +0100

    sozial

--- main/assets/sozial.php
+++ main/assets/sozial.php
@@ -1,42 +1,36 @@
 <?php
 // assets/sozial.php
 ?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <section id="sozial" class="py-5 text-center">
   <div class="container">
     <h2 class="mb-4">Sozial Media</h2>
     <ul class="social-list" role="list">
+
       <li role="listitem">
-        <a class="social-link" href="https://www.instagram.com/juliuspollag/" target="_blank" rel="noopener noreferrer" title="Instagram">
-          <span class="social-label">Instagram</span>
-          <span class="social-arrow" aria-hidden="true">↗</span>
+        <a href="https://www.instagram.com/juliuspollag/" class="fa fa-instagram"></a>
         </a>
       </li>
 
       <li role="listitem">
-        <a class="social-link" href="https://github.com/20Rayzel03" target="_blank" rel="noopener noreferrer" title="GitHub">
-          <span class="social-label">GitHub</span>
-          <span class="social-arrow" aria-hidden="true">↗</span>
+        <a href="https://github.com/20Rayzel03" class="fa fa-github"></a>
         </a>
       </li>
 
       <li role="listitem">
-        <a class="social-link" href="https://www.linkedin.com/in/julius-pollag-2a9b3b2bb/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
-          <span class="social-label">LinkedIn</span>
-          <span class="social-arrow" aria-hidden="true">↗</span>
+        <a href="https://www.linkedin.com/in/julius-pollag-2a9b3b2bb/" class="fa fa-linkedin"></a>
         </a>
       </li>
 
       <li role="listitem">
-        <a class="social-link" href="https://steamcommunity.com/profiles/76561199027887859/" target="_blank" rel="noopener noreferrer" title="Steam">
-          <span class="social-label">Steam</span>
-          <span class="social-arrow" aria-hidden="true">↗</span>
+        <a href="#" class="fa fa-facebook "></a>
         </a>
       </li>
 
       <li role="listitem">
-        <a class="social-link" href="mailto:julius.pollag@jqpollag.de" title="E-Mail">
-          <span class="social-label">E-Mail</span>
-          <span class="social-arrow" aria-hidden="true">↗</span>
+        <a href="mailto:julius.pollag@jqpollag.de" class="fa fa-mailto"></a>
         </a>
       </li>
     </ul>
